65420c6494c97f09a37f60127c69aa3f1805d8a1
jcalla19 <jtcallahan02@gmail.com>
2025-11-19 20:41:18 -0500

    deploy
    deploy

--- main.tf
+++ main.tf
@@ -85,7 +85,7 @@ resource "azurerm_mssql_database" "db" {
   server_id      = azurerm_mssql_server.sql.id
   collation      = "SQL_Latin1_General_CP1_CI_AS"
   license_type   = "LicenseIncluded"
-  max_size_gb    = 4
+  max_size_gb    = 1
   read_scale     = true
   sku_name       = "S0"
   zone_redundant = true
