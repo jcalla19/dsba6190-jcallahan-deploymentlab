953a376f4ada7122d45fa2d4a014758980e19e46
jcalla19 <jtcallahan02@gmail.com>
2025-11-19 19:13:18 -0500

    Deploy
    Deploy

--- variables.tf
+++ variables.tf
@@ -37,7 +37,7 @@ variable "environment" {
 variable "student_name" {
   description = "Application Name"
   type        = string
-  default     = "jcall"
+  default     = "jcal"
 }
 
 variable "class_name" {
